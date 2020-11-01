242cc57eeaa167dc7c1769cde600b5dfcc55cf59
krpk1900 <krpk1900@gmail.com>
2020-11-01 09:11:32 +0000

    Finish Chapter 4

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'slim-rails'
 gem 'html2slim'
 #gem 'bootstrap-sass'
 gem 'bootstrap'
+gem 'rails_autolink'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
--- db/migrate/20201101075717_add_user_id_to_tasks.rb
+++ db/migrate/20201101075717_add_user_id_to_tasks.rb
@@ -1,6 +1,6 @@
 class AddUserIdToTasks < ActiveRecord::Migration[5.1]
   def up
-    execute 'DELETE FROM tasks:'
+    execute 'DELETE FROM tasks;'
     add_reference :tasks, :user, null: false, index: true
   end
   
